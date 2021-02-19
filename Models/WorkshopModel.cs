fbb7598c6ece8d9ffdf3c350b47626c2f0ca70b0
carlos-manuel-luna <carlos.luna@ucb.edu.bo>
2021-02-18 22:03:09 -0400

    Add workshop's model and status enum for workshop

---
+++ Models/WorkshopModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop_api.Models
+{
+    public class WorkshopModel
+    {
+        public string id;
+        public string name;
+        public WorkshopStatesEnum status;
+    //    (Id, Name, Status(Scheduled/Postponed/Cancelled)) - 
+    }
+}
---
+++ Models/WorkshopStatesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop_api.Models
+{
+    public enum WorkshopStatusEnum
+    {
+        SCHEDULED,
+        POSTPONED,
+        CANCELLED
+    }
+}
